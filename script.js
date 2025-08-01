22d1dc96265dea5d2e1eda793b4c56d837844405
SHEMA IRADUKUNDA Daniel <danieliradukunda00@gmail.com>
2025-08-01 19:08:33 +0200

    correcting username typo

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
   form.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    const name = document.getElementById("username").value.trim();
+    const username = document.getElementById("username").value.trim();
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value.trim();
 
     let isValid = true;
     const messages = [];
 
-    if (name.length < 3) {
+    if (username.length < 3) {
       isValid = false;
       messages.push("Name must be at least 3 characters long.");
     }
